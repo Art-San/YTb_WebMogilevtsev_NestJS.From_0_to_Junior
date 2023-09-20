5b548bc32f5e3f7c7705ab137d70b36081bea287
Aleksander <asava@sibmail.com>
2023-09-20 20:54:11 +0700

    nest_g_resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
 import { TaskModule } from './task/task.module'
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TaskModule],
+  imports: [TaskModule, UsersModule],
 })
 export class AppModule {}
